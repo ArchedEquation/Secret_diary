36f995b07750ae0db8a1c3397392d86b4926bbdd
ArchedEquation <praneethyeddu@gmail.com>
2023-12-14 09:46:04 +0530

    starting a new project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 56, 226, 9)),
+            
+      ),
+    );
+  }
+}
